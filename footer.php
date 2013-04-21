e43fe1e8e15363ca0788825d869a2c74e6c4709b
osmltir <ldelevallez@gmail.com>
2013-04-22 00:51:08 +0200

    modif css

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="style.css"/>
+        <title>Lomme Team</title>
+        <link rel="icon" href="img/favicon.ico" type="image/ico" /> <!-- A faire -->    
+    </head>
+    <body>
+        <footer>
+            <a href="https://www.facebook.com/pages/OSM-Lomme-Section-Tir/120681891293851"><img src="img/like.png"></a>
+        </footer>
+    </body>
+</html>
