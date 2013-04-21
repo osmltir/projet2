d354aaf104f3418d2e71317e90e270222347a029
osmltir <ldelevallez@gmail.com>
2013-04-21 23:22:54 +0200

    ajout

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="style.css"/>
+        <title>Lomme Team</title>
+        <link rel="icon" href="img/favicon.ico" type="image/ico" /> <!-- A faire -->    
+    </head>
+    <body>
+        <header>
+            <div class="titre">
+                <p>BIENVENUE SUR <br/> LE SITE OFFICIEL DE L'OSM LOMME TIR</p>
+            </div>
+        </header>
+        <section>
+            <article>
+                <!-- logo LOMME TEAM en CSS -->
+            </article>
+            <a href="famille.php"><div class="icone" id="famille"></div></a>
+            <a href="tireur.php"><div class="icone" id="tireur"></div></a>
+            <a href="entreprise.php"><div class="icone" id="entreprise"></div></a>
+            <div class="cadre" id="famille1">Accueil Famille</div>
+            <div class="cadre" id="tireur1">Accueil Tireur</div>
+            <div class="cadre" id="entre1">Accueil Entreprise</div>
+        </section>
+    </body>
+</html>
