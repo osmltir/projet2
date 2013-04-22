e806c3a9bdab37fa10b680a2a212077cc0fcc2e1
osmltir <ldelevallez@gmail.com>
2013-04-22 11:14:37 +0200

    ajout page "nous"

--- nous.php
+++ nous.php
@@ -9,7 +9,15 @@
     <body>
         <section>
             <article>
-                <p><h1>Notre politique interne</h1>L’OSML SECTION TIR à la chance d’avoir à sa tête le plus jeune conseil d’administration de France ! En effet avec 30 ans de moyenne d’âge, l’asso obtient la première place mais attention aux apparances, c’est l’un des clubs ayant la plus forte courbe de croissance et d’évolution.</p>
+                <p><h1>Notre politique interne</h1>
+                L’OSML SECTION TIR à la chance d’avoir à sa tête le plus jeune 
+                conseil d’administration de France ! En effet avec 30 ans de 
+                moyenne d’âge, l’asso obtient la première place mais attention 
+                aux apparances, c’est l’un des clubs ayant la plus forte courbe 
+                de croissance et d’évolution. <br/>
+                <br/>
+                <h3>Nous basons notre développement sur 3 axes prioritaires :</h3>
+                </p>
             </article>
         </section>
     </body>
