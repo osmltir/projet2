487ac94997dc3745c17a5782164e848c6fe734cd
osmltir <ldelevallez@gmail.com>
2013-04-22 11:00:28 +0200

    ajout page "nous"

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" href="style.css"/>
+        <link rel="stylesheet" href="style1.css"/>
         <title>Lomme Team</title>
         <link rel="icon" href="img/favicon.ico" type="image/ico" /> <!-- A faire -->    
     </head>
---
+++ nous.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="style.css"/>
+        <title>Lomme Team</title>
+        <link rel="icon" href="img/favicon.ico" type="image/ico" /> <!-- A faire -->    
+    </head>
+    <body>
+        <section>
+            <article>
+                <p><h1>Notre politique interne</h1>L’OSML SECTION TIR à la chance d’avoir à sa tête le plus jeune conseil d’administration de France ! En effet avec 30 ans de moyenne d’âge, l’asso obtient la première place mais attention aux apparances, c’est l’un des clubs ayant la plus forte courbe de croissance et d’évolution.</p>
+            </article>
+        </section>
+    </body>
+</html>
