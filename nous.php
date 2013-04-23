303819ff56aac7a1761d804b13535397e7d32c30
osmltir <ldelevallez@gmail.com>
2013-04-23 13:56:22 +0200

    modification "nous"

--- nous.php
+++ nous.php
@@ -9,30 +9,53 @@
     <body>
         <section>
             <article>
-                <p>
-                <h1>Notre politique interne</h1>
-                L’OSML SECTION TIR à la chance d’avoir à sa tête le plus jeune 
-                conseil d’administration de France ! En effet avec 30 ans de 
-                moyenne d’âge, l’asso obtient la première place mais attention 
-                aux apparances, c’est l’un des clubs ayant la plus forte courbe 
-                de croissance et d’évolution. <br/>
-                <br/>
-                <h3>Nous basons notre développement sur 3 axes prioritaires :</h3>
-                - la promotion: Leader dans ce domaine! édition de 10.000
-                flyers tous les 2 ans chez imprimeurs, l’impression d’une centaine 
-                d’affiches, de centaines de tracts diffusés à domicile, dans les
-                batiments municipaux, nombreuses parutions dans les médias, une 
-                gamme d’articles publicitaires variés (voir la boutique) ... , 
-                soit plusieurs centaines d’euros alloués par an! 
-                - l’accessibilité du tir à tous: jeune avec une école de
-                tir labellisée ESU, femme, personne en difficulté, personne 
-                handicapé, nous ne laissons personne de côté et équipons chacun 
-                de nos membres avec du matériel dernier cris. 
-                - le développement de nouvelles disciplines afin d’offrir
-                à tous nos adhérents la possibilité de pratiquer les disciplines de 
-                leurs choix. Développement du tir à l’arbalète Field, l’arbalète
-                Match, le pistolet standart, le pistolet vitesse, la cible mobile, ... .
-                </p>
+                <div id="t1">
+                    <h1>Notre politique interne</h1>
+                    L’OSML SECTION TIR à la chance d’avoir à sa tête le plus jeune 
+                    conseil d’administration de France ! En effet avec 30 ans de 
+                    moyenne d’âge, l’asso obtient la première place mais attention 
+                    aux apparances, c’est l’un des clubs ayant la plus forte courbe 
+                    de croissance et d’évolution.<br/>
+                    <br/>
+                    <h3>Nous basons notre développement sur 3 axes prioritaires :</h3>
+                    <b>- la promotion</b>: Leader dans ce domaine! édition de 10.000
+                    flyers tous les 2 ans chez imprimeurs, l’impression d’une centaine 
+                    d’affiches, de centaines de tracts diffusés à domicile, dans les
+                    batiments municipaux, nombreuses parutions dans les médias, une 
+                    gamme d’articles publicitaires variés (voir la boutique) ... , 
+                    soit plusieurs centaines d’euros alloués par an!<br/>
+                    <b>- l’accessibilité du tir à tous</b>: jeune avec une école de
+                    tir labellisée ESU, femme, personne en difficulté, personne 
+                    handicapé, nous ne laissons personne de côté et équipons chacun 
+                    de nos membres avec du matériel dernier cris.<br/>
+                    <b>- le développement de nouvelles disciplines</b> afin d’offrir
+                    à tous nos adhérents la possibilité de pratiquer les disciplines de 
+                    leurs choix. Développement du tir à l’arbalète Field, l’arbalète
+                    Match, le pistolet standart, le pistolet vitesse, la cible mobile, ... .<br/>
+                    <h3>De nombreux partenaires...</h3>
+                    Pour réaliser nos projets et ambitions, nous sommes principalement
+                    aidés financièrement et matériellement par notre commune de Lomme,
+                    le Ministère de la Jeunesse et des Sports, le Conseil Général et 
+                    le Conseil Régional. Cependant nous cherchons constamment d’autres
+                    financeurs, mécènes ou sponsors capables de nous aider d’avantages dans nos projets.
+                    <h3>Un investissement massif!</h3>
+                    Démocratiser le tir sportif, c’est offrir à tous la chance d’avancer
+                    d’aller le plus loin possible. Ce n’est pas prêter du matériel
+                    trentenaire mais équiper ces adhérents avec du matériel dernier cri.
+                    Cela passe par un investissement massif permettant de rendre ce sport
+                    réellement accéssible. Pour évoluer un licencier doit être équipé
+                    avec tout le matériel nécessaire (veste, pantalon, gant, arme,...)
+                    mais surtout du matériel de qualité soit près de 2000€ pour une carabine,
+                    1300€ pour un pistolet, 2000€ pour une arbalète, 150€ pour une veste,... ,
+                    auquels viennent s’ajouter un surcoût annuel de 500€/compétiteur 
+                    (déplacement, frais d’inscription, hébergement,...). Le coût d’un adhérent
+                    peut donc vite monter à plusieurs centaines d’euros/an. 
+                    Peu de personnes peuvent se permettre de payer autant d’argent chaque année
+                    pour pratiquer un sport ou une passion, l’OSML SECTION TIR a donc décider de
+                    financer tout cela. Nous avons donc investit et investissons plusieurs
+                    dizaines de milliers d’euros pour offrir à nos adhérents Lommois la chance ce
+                    concourir dans les meilleurs conditions.
+                </div>
             </article>
         </section>
     </body>
